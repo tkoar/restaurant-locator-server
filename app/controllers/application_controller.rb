fbdd08f7b1a43db9874497cb9e734c6eccf654e1
tkoar <terrancekoar@gmail.com>
2018-07-17 20:17:16 -0400

    moving index settings to rails server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::API
 
+  helper_method :configure_algolia
+
   def configure_algolia
-    
+    Algolia.init(application_id: ENV[API_ID], api_key: ENV['ADMIN_API_KEY'])
+    index = Algolia::Index.new('open_table_restaurants')
+    return Algolia
   end
 
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,24 @@
 class AuthController < ApplicationController
 
+  def configure_index
+    index = Algolia::Index.new('open_table_restaurants')
+    begin
+      index.set_settings({
+        'attributesForFaceting': ['food_type'],
+        'sortFacetValuesBy': 'count',
+        'searchableAttributes': ["name","food_type","city","area","neighborhood"],
+        'ranking': ['typo','geo','words','attribute','proximity','exact','custom'],
+        'hitsPerPage': 1000,
+        'paginationLimitedTo': 5000
+      })
+    rescue
+      render json: {"ERROR": "Failed to set index settings"}
+    end
+  end
+
   def generate
+    configure_index
+    byebug
     render json: {secure_key: Algolia.generate_secured_api_key(ENV['API_KEY'], [])}
   end
 
